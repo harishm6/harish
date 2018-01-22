739d319357fa8dd8a91d30235d31d4f8f0d6b569
harishm6 <35329110+harishm6@users.noreply.github.com>
2018-01-22 04:48:46 -0800

    Add files via upload

---
+++ bit_extract.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int a,i,temp1,temp2,count=0;
+	scanf("%d",&a);
+	for(i=15;i>=0;i--)
+	{
+		printf("%d ",((a&(1<<i))==(1<<i)));
+		count++;
+		if(count==8)
+		{
+			printf("  ");
+			count=0;
+		}		
+	}
+	printf("\n");
+	temp1=a&0x00ff;
+	temp2=a&0xff00;
+	printf("%d ",temp1);
+	printf("%d ",temp2);
+	a=temp1|temp2;
+	printf("\n");
+	printf("%d",a);
+}
